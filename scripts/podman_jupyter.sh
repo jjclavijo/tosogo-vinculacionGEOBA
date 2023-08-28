88f3be667dcd98b8f119b09a4730204510314416
Javier Clavijo <jjclavijo@gmail.com>
2023-08-28 13:06:51 -0300

    Corregido Pod

--- scripts/podman_jupyter.sh
+++ scripts/podman_jupyter.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-podman pod create --name=DP-Vinculacion --share net -p 127.0.0.1:8890:8890
-podman run --rm --name=DP-Vinculacion_notebooks_1 --pod=DP-Vinculacion \
+podman pod create --name=Vinculacion --share net -p 127.0.0.1:8890:8890 \
+                  --add-host notebooks:127.0.0.1 --add-host Vinculacion_notebooks_1:127.0.0.1
+podman run --rm --name=Vinculacion_notebooks_1 --pod=Vinculacion \
               -e LOCAL_USER_ID=1000 \
               --mount type=bind,source="$(realpath .)",destination=/proyecto \
               --mount type=bind,source="/home/javier/proy/tosogo/glue",destination=/glue \
-              --add-host notebooks:127.0.0.1 --add-host DP-Vinculacion_notebooks_1:127.0.0.1 \
               docker.io/jjclavijo/tosogo:latest jupyter-lab --allow-root --ip=0.0.0.0 --port=8890 --no-browser --notebook-dir=/proyecto/notebooks
+
+podman pod rm Vinculacion
